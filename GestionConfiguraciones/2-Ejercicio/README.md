68c63af21ae91e02a88a820285718428c3e03ce7
Marvin M. Agüero T <maguero@correo.ugr.es>
2016-11-24 17:55:04 +0100

    Agregando ejercicios Hito 2. References 3

---
+++ GestionConfiguraciones/2-Ejercicio/README.md
@@ -0,0 +1,2 @@
+#Ejercicio 2
+Crear una receta para instalar nginx, tu editor favorito y algún directorio y fichero que uses de forma habitual.
---
+++ GestionConfiguraciones/2-Ejercicio/chef-solo/cookbooks/emacs/recipes/default.rb
@@ -0,0 +1,9 @@
+package 'emacs'
+directory '/home/mmaguero/Documentos'
+file "/home/mmaguero/Documentos/LEEME" do
+	owner "mmaguero"
+	group "mmaguero"
+	mode 00544
+	action :create
+	content "Directorio para documentos diversos"
+end
